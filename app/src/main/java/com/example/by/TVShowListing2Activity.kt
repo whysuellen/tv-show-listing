bdf4c32f6b4670573809fd540699a31626b1ca2b
leonardorpaixao <leonardorpaixao@gmail.com>
2023-02-01 16:24:58 -0300

    feat: tv show model and navigation passing through intent information

--- app/src/main/java/com/example/by/TVShowListing2Activity.kt
+++ app/src/main/java/com/example/by/TVShowListing2Activity.kt
@@ -4,9 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.by.databinding.Tvshowlistingactivity2LayoutBinding
-import com.example.by.tvShows.ThePunisherActivity
+import com.example.by.domain.models.TvShow
+import com.example.by.tvShows.TvShowDetailsActivity
 
-class TVShowListing2Activity : AppCompatActivity (){
+class TVShowListing2Activity : AppCompatActivity() {
 
     private lateinit var binding: Tvshowlistingactivity2LayoutBinding
 
@@ -19,8 +20,16 @@ class TVShowListing2Activity : AppCompatActivity (){
 
     private fun setupView() {
         binding.buttonThePunisher.setOnClickListener {
-            val intent = Intent(this, ThePunisherActivity::class.java)
-            startActivity(intent)
+            val thePunisherTvShow = TvShow(
+                title = "The Punisher",
+                image = R.drawable.the_punisher,
+                age = "2017",
+                description = "Em O Justiceiro, o crime não dura quando Frank Castle está por perto. O veterano de guerra é impiedoso na sua guerra contra malfeitores, alimentada pelo ódio gerado quando sua família foi pega no fogo cruzado durante um tiroteio entre mafiosos."
+            )
+            val intent = Intent(this, TvShowDetailsActivity::class.java).apply {
+                putExtra("chave_aleatoria", thePunisherTvShow)
             }
+            startActivity(intent)
         }
     }
+}
---
+++ app/src/main/java/com/example/by/domain/models/TvShow.kt
@@ -0,0 +1,8 @@
+package com.example.by.domain.models
+
+data class TvShow(
+    val title: String,
+    val image: Int,
+    val age: String,
+    val description: String,
+): java.io.Serializable
